50d0ba202853e78b8da40b440cfc3233fd162d9d
SCSL-22 <SCSL-22@DESKTOP-K8JVKHG>
2024-05-02 13:06:19 +0600

    Add project files.

---
+++ Hospital.Server/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Hospital.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Server.Data
+{
+    public class AppDbContext: DbContext
+    {
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        { }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
